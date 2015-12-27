b47a715c70dfdaa33270214f66fcf21e907707a4
unknown <aaron.cremona@GEDULAP-AC.EDUPRIZE.LOCAL>
2015-12-27 16:18:53 -0700

    added zero time remaining check, work cycle counter, and state flip

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 var p = new Pomodoro;
 
-p.init(25, 5);
+p.init(.25, .25);
 p.start();
--- js/pomodoro.js
+++ js/pomodoro.js
@@ -3,7 +3,8 @@ function Pomodoro() {
 		breakTime,
 		timeRemaining,
 		startTime,
-		state = 'work';
+		state = 'work',
+		workCycles = 0;
 
 	function setWork (time) {
 		workTime = time * 60 * 1000;
@@ -15,8 +16,9 @@ function Pomodoro() {
 
 	function toMinSec(time)  {
 		time = time / 1000;
-		var sec = Math.floor(time % 60);
+		var sec = "0" + Math.floor(time % 60);
 		var min = Math.floor(time / 60);
+		sec = sec.slice(-2);
 		return min + ":" + sec;
 	}
 
@@ -25,16 +27,22 @@ function Pomodoro() {
 		if (state === 'work') {
 			timeRemaining = workTime - timeElapsed;
 		}
-		if (state === 'break') {
+		else if (state === 'break') {
 			timeRemaining = breakTime - timeElapsed;
 		}
-		logEverything();
-	};
 
-	this.start = function() {
-		startTime = Date.now();
-		var timer = setInterval(update, 1000);
-		state = 'work';
+		if (timeRemaining <= 0) {
+			startTime = Date.now();
+			if (state === 'work') {
+				workCycles++;
+				state = 'break';
+			}
+			else if (state === 'break') {
+				state = 'work';
+			}
+		}
+
+		logEverything();
 	};
 
 	this.init = function (workTime, breakTime) {
@@ -42,11 +50,17 @@ function Pomodoro() {
 		setBreak(breakTime);
 	};
 
+	this.start = function() {
+		startTime = Date.now();
+		var timer = setInterval(update, 500);
+	};
+
 	function logEverything() {
 		console.log("work time: " + toMinSec(workTime) +
 			"\t\tbreak time: " + toMinSec(breakTime) +
 			"\t\tstart time: " + startTime +
 			"\t\ttime remaining: " + toMinSec(timeRemaining) +
-			"\t\tstate: " + state);
+			"\t\tstate: " + state +
+			"\t\twork cycles: " + workCycles);
 	};
 }
